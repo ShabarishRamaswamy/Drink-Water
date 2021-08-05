d3073b716be06ab4710fddc831f6893bafc64abd
ShabarishRamaswamy <shabarishramaswamy@gmail.com>
2021-08-05 08:11:38 +0530

    Badge text updates at interval

--- background.js
+++ background.js
@@ -1,4 +1,19 @@
+delayInMinutes = 0.1;
 chrome.alarms.create("DrinkWater", { delayInMinutes: 1, periodInMinutes: 60 });
 chrome.alarms.onAlarm.addListener((alarm) => {
     console.log("Alarm fired")
 })
+/**
+ * Timer used for the interval
+ */
+
+setInterval(() => {
+    chrome.action.setBadgeBackgroundColor(
+        { color: [255, 0, 0, 0] },  // Green
+        () => { /* ... */ },
+    );
+
+    chrome.action.setBadgeText(
+        { text: "!" }
+    );
+}, delayInMinutes * 1000 * 60);
