7bef36f6797f8f7592eebba480d339e245ed2e36
shabarishramaswamy <shabarishramaswamy@gmail.com>
2021-08-19 22:55:35 +0530

    Allows updation of Timer duration and Timer type from popup HTML.

--- scripts/popup.js
+++ scripts/popup.js
@@ -3,16 +3,29 @@
  */
 var interval_input = document.getElementsByClassName("settings_interval_input")[0];
 var timer_type_input = document.getElementsByClassName("timer_type_input")[0];
-var timer = document.getElementsByClassName("timer")[0];
+var timer_type_select = document.getElementsByClassName("timer_type_input")[1];
+var timer_fg = document.getElementsByClassName("timer-fg")[0];
+var update_button = document.getElementsByClassName("updateTimer")[0];
 
 
 chrome.storage.sync.get(["delayInMinutes", "periodInMinutes", "timerType"], (values) => {
     interval_input.value = values.periodInMinutes;
     timer_type_input.value = values.timerType;
-    timer.style["animation-duration"] = `${values.periodInMinutes*60}s`
+    timer_fg.style["animation-duration"] = `${values.periodInMinutes*60}s`
 });
 
-
+update_button.addEventListener('click', () => {
+    if(timer_type_select.options[timer_type_select.selectedIndex].value){
+        chrome.storage.sync.set({
+            "timerType": timer_type_select.options[timer_type_select.selectedIndex].value
+        })
+    }
+    if(interval_input.value){
+        chrome.storage.sync.set({
+            "periodInMinutes": interval_input.value
+        })
+    }
+})
 
 
 /**
