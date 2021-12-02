86616b15dc4ae7c7f7323259d5ef875fc0448e54
Shabarish <shabarishramaswamy@gmail.com>
2021-12-02 23:19:10 +0530

    Added comms between popup and background.

--- background.js
+++ background.js
@@ -1,42 +1,56 @@
 /**
  * Setting default values in the Storage which can be changed.
  */
-chrome.storage.sync.set({
-    "delayInMinutes": 0,
-    "periodInMinutes": 1,
-    "timerType": "Clock"
-}, () => {
-    chrome.storage.sync.get(["delayInMinutes", "periodInMinutes"], (values) => {
-        createTimer(values.delayInMinutes, values.periodInMinutes);
-    });
-});
+chrome.storage.sync.set(
+    {
+        delayInMinutes: 0,
+        periodInMinutes: 1,
+        timerType: "Clock",
+    },
+    () => {
+        chrome.storage.sync.get(
+            ["delayInMinutes", "periodInMinutes"],
+            (values) => {
+                createTimer(values.delayInMinutes, values.periodInMinutes);
+            }
+        );
+    }
+);
 
 /**
  * @function creates timer to be used as the main timer of the extension.
  */
 createTimer = (delayInMinutes, periodInMinutes) => {
-    chrome.alarms.create( "DrinkWater",{ 
-        delayInMinutes: delayInMinutes, 
-        periodInMinutes: periodInMinutes 
+    chrome.alarms.create("DrinkWater", {
+        delayInMinutes: delayInMinutes,
+        periodInMinutes: periodInMinutes,
     });
-}
-
+};
 
 chrome.alarms.onAlarm.addListener((alarm) => {
-
     chrome.action.setBadgeBackgroundColor(
-        { color: [255, 0, 0, 0] },  // Blue
-        () => { /* ... */ },
+        { color: [255, 0, 0, 0] }, // Blue
+        () => {
+            console.log("Timer over !");
+        }
     );
 
-    chrome.action.setBadgeText(
-        { text: "!" }
-    );
+    chrome.action.setBadgeText({ text: "!" });
 
     console.log("Alarm fired");
-})
+});
 
 /**
- * Now, when the user clicks the plugin and says that they have consumed water, we must reset the 
+ * Now, when the user clicks the plugin and says that they have consumed water, we must reset the
  * badge and the exclaimation on the plugin's icon.
  */
+chrome.runtime.onMessage.addListener((request, sender) => {
+    if (request.iconClicked) {
+        console.log("Hi");
+        chrome.action.getBadgeText((text) => {
+            if (text == "!") {
+                chrome.action.setBadgeText("");
+            }
+        });
+    }
+});
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,32 +1,42 @@
 /**
  * All the Required Variables
  */
-var interval_input = document.getElementsByClassName("settings_interval_input")[0];
+var interval_input = document.getElementsByClassName(
+    "settings_interval_input"
+)[0];
+
 var timer_type_input = document.getElementsByClassName("timer_type_input")[0];
 var timer_type_select = document.getElementsByClassName("timer_type_input")[1];
 var timer_fg = document.getElementsByClassName("timer-fg")[0];
 var update_button = document.getElementsByClassName("updateTimer")[0];
 
+chrome.runtime.sendMessage({ iconClicked: "Hi" });
 
-chrome.storage.sync.get(["delayInMinutes", "periodInMinutes", "timerType"], (values) => {
-    interval_input.value = values.periodInMinutes;
-    timer_type_input.value = values.timerType;
-    timer_fg.style["animation-duration"] = `${values.periodInMinutes*60}s`
+chrome.storage.sync.get(
+    ["delayInMinutes", "periodInMinutes", "timerType"],
+    (values) => {
+        interval_input.value = values.periodInMinutes;
+        timer_type_input.value = values.timerType;
+        timer_fg.style["animation-duration"] = `${
+            values.periodInMinutes * 60
+        }s`;
+    }
+);
+
+update_button.addEventListener("click", () => {
+    if (timer_type_select.options[timer_type_select.selectedIndex].value) {
+        chrome.storage.sync.set({
+            timerType:
+                timer_type_select.options[timer_type_select.selectedIndex]
+                    .value,
+        });
+    }
+    if (interval_input.value) {
+        chrome.storage.sync.set({
+            periodInMinutes: interval_input.value,
+        });
+    }
 });
-
-update_button.addEventListener('click', () => {
-    if(timer_type_select.options[timer_type_select.selectedIndex].value){
-        chrome.storage.sync.set({
-            "timerType": timer_type_select.options[timer_type_select.selectedIndex].value
-        })
-    }
-    if(interval_input.value){
-        chrome.storage.sync.set({
-            "periodInMinutes": interval_input.value
-        })
-    }
-})
-
 
 /**
  * Message Passing code.
