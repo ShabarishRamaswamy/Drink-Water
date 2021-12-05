373c07a1bc076270b2983ca92690ef8b291403ce
Shabarish <shabarishramaswamy@gmail.com>
2021-12-05 23:06:33 +0530

    User can now change the alarm time.

--- background.js
+++ background.js
@@ -20,11 +20,20 @@ chrome.storage.sync.set(
 /**
  * @function creates timer to be used as the main timer of the extension.
  */
+chrome.storage.onChanged.addListener((changes, namespace) => {
+    for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+        if (key == "periodInMinutes") {
+            createTimer(0, Number(newValue));
+        }
+    }
+});
+
 createTimer = (delayInMinutes, periodInMinutes) => {
     chrome.alarms.create("DrinkWater", {
         delayInMinutes: delayInMinutes,
         periodInMinutes: periodInMinutes,
     });
+    console.log("Created Timer");
 };
 
 chrome.alarms.onAlarm.addListener((alarm) => {
@@ -40,33 +49,33 @@ chrome.alarms.onAlarm.addListener((alarm) => {
     chrome.storage.sync.set({ badgeText: "!" });
 
     console.log("Alarm fired");
-});
 
-/**
- * Now, when the user clicks the plugin and says that they have consumed water, we must reset the
- * badge and the exclaimation on the plugin's icon.
- */
-chrome.runtime.onMessage.addListener((request, sender) => {
-    if (request.iconClicked) {
-        console.log("Hi");
-        try {
-            // TODO: getBadgeText Does not work.
-            // chrome.action.getBadgeText(null, (text) => {
-            //     if (text == "!") {
-            //         chrome.action.setBadgeText("");
-            //     }
-            // });
+    /**
+     * Now, when the user clicks the plugin and says that they have consumed water, we must reset the
+     * badge and the exclaimation on the plugin's icon.
+     */
+    chrome.runtime.onMessage.addListener((request, sender) => {
+        if (request.iconClicked) {
+            console.log("Hi");
+            try {
+                // TODO: getBadgeText Does not work.
+                // chrome.action.getBadgeText(null, (text) => {
+                //     if (text == "!") {
+                //         chrome.action.setBadgeText("");
+                //     }
+                // });
 
-            // Temporary workaround
-            chrome.storage.sync.get("badgeText", (res) => {
-                console.log(`Badge Text: ${res.badgeText}`);
-                if (res.badgeText == "!") {
-                    chrome.action.setBadgeText({ text: "" });
-                }
-            });
-        } catch (e) {
-            console.log(e);
-            console.log("No Badge.");
+                // Temporary workaround
+                chrome.storage.sync.get("badgeText", (res) => {
+                    console.log(`Badge Text: ${res.badgeText}`);
+                    if (res.badgeText == "!") {
+                        chrome.action.setBadgeText({ text: "" });
+                    }
+                });
+            } catch (e) {
+                console.log(e);
+                console.log("No Badge.");
+            }
         }
-    }
+    });
 });
