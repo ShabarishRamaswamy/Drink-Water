df1029dd38c9c96de58a6f7a56c14a5bc7550d51
Shabarish <shabarishramaswamy@gmail.com>
2021-12-31 21:22:57 +0530

    V1 frontend ready

--- scripts/popup.js
+++ scripts/popup.js
@@ -123,6 +123,16 @@ var displayTimer = (currentTime) => {
 };
 
 /**
+ * Removes Scroll bar while retaining scrolling.
+ */
+var styleElement = document.createElement("style");
+styleElement.id = "remove-scroll-style";
+styleElement.textContent =
+    "html::-webkit-scrollbar{display:none !important}" +
+    "body::-webkit-scrollbar{display:none !important}";
+document.getElementsByTagName("body")[0].appendChild(styleElement);
+
+/**
  * Message Passing code.
  */
 
